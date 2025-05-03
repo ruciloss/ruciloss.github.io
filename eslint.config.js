38204ec7941e3e493503ecedddc1f76338e68201
Ruciloss <rucilos96@gmail.com>
2025-05-03 02:42:25 +0200

    update

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: [
+        "plugin:@next/next/recommended",
+        "next/core-web-vitals",
+        "next/typescript",
+    ],
+    plugins: ["@stylexjs"],
+    rules: {
+        "@stylexjs/valid-styles": "error",
+    },
+};
