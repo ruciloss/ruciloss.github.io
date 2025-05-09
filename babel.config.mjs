32792d2284a35ea50ed3d2290de73b9898879343
Ruciloss <rucilos96@gmail.com>
2025-05-09 02:51:14 +0200

    babel test

--- babel.config.mjs
+++ babel.config.mjs
@@ -1,30 +1,33 @@
-import path from 'path';
+import { fileURLToPath } from "url";
+import { join } from "path";
 
-export default function(api) {
-  api.cache(true);
+const dirname = fileURLToPath(new URL(".", import.meta.url));
 
-  const presets = ['next/babel'];
-  const plugins = [
-    [
-      '@stylexjs/babel-plugin',
-      {
-        dev: process.env.NODE_ENV === 'development',
-        test: process.env.NODE_ENV === 'test',
-        runtimeInjection: false,
-        treeshakeCompensation: true,
-        // classNamePrefix: "y",
-        aliases: {
-          '@/*': [path.join(__dirname, '*')],
-        },
-        unstable_moduleResolution: {
-          type: 'commonJS',
-        },
-      },
-    ],
-  ];
+export default function (api) {
+    api.cache(true);
 
-  return {
-    presets,
-    plugins,
-  };
-}
+    const presets = ["next/babel"];
+    const plugins = [
+        [
+            "@stylexjs/babel-plugin",
+            {
+                dev: process.env.NODE_ENV === "development",
+                test: process.env.NODE_ENV === "test",
+                runtimeInjection: false,
+                treeshakeCompensation: true,
+                // classNamePrefix: "y",
+                aliases: {
+                    "@/*": [join(dirname, "*")],
+                },
+                unstable_moduleResolution: {
+                    type: "commonJS",
+                },
+            },
+        ],
+    ];
+
+    return {
+        presets,
+        plugins,
+    };
+};
